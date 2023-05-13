31be1c136c43ea56f99fec1a887e5627878b0c25
Nicolò Tagliaferro <nico.taglia26@gmail.com>
2023-05-13 16:11:11 +0200

    Posts on index page

--- public/index.php
+++ public/index.php
@@ -6,16 +6,14 @@ require_once 'check_auth.php';
 <body>
 <?php include '../tools/menu.php'; ?>
     <h1> Welcome <?php echo $_SESSION['username'] ?>! </h1>
-    <label>
-        <input type="text" name="url" placeholder="Enter image url"/>
-    </label>
-    <input type="submit" value="Add image" id="add_img"/>
 <?php
-$images = $DB->query("SELECT * FROM photos order by id desc");
-foreach ($images as $image) {
+$posts = $DB->query("SELECT posts.*, username FROM posts inner join users u on u.id = posts.user_id order by posts.id desc");
+foreach ($posts as $post) {
     ?>
-    <div class="image">
-        <img src='<?php echo $image['url'] ?>' alt="image"/>
+    <div class="post">
+        <h3>@<?php echo $post['username']; ?></h3>
+        <img src="/images/<?php echo $post['image_path']; ?>"/>
+        <p><?php echo $post['description']; ?></p>
     </div>
     <?php
 }
