367d287e8829215327538f9010afbd35bfb9d737
DaMole98 <salvaterra.damiano@gmail.com>
2023-05-13 10:53:12 +0200

    registrazione

--- public/login.php
+++ public/login.php
@@ -1,38 +1,21 @@
 <?php
 require_once("../tools/db_conn.php");
 global $DB;
-/*
-login page
- */
-?>
+session_start();
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Login</title>
-    </head>
-
-<body>
-<?php
-
-    session_start();
-
-    if(isset($_POST['username']) && isset($_POST['password'])) {
-        $username = stripslashes($_POST['username']);
-        $password = stripslashes($_POST['password']);
+if(isset($_POST['submit'])) {
+    $username = stripslashes($_POST['username']);
+    $password = stripslashes($_POST['password']);
 
 
-        $query = $DB->query("SELECT * FROM 'users' WHERE username='$username' and password = '$password'");
-        $qresult = $query -> fetch(PDO::FETCH_ASSOC);
+    $query = $DB->query("SELECT * FROM 'users' WHERE username='$username' and password = '$password'");
+    $qresult = $query->fetch(PDO::FETCH_ASSOC);
 
-        if(!($qresult)){
-            echo "Username/password is incorrect. Click <a href=". "" . "login.php>here</a> to retry";
-            exit();
-        }
+    if (!($qresult)) {
+        echo "Username/password is incorrect.";
 
+    } else {
         $UID = $qresult["id"];
-
         if (!(is_null($UID))) {
             $_SESSION['id'] = $UID;
             $_SESSION['username'] = $qresult['username'];
@@ -40,29 +23,31 @@ login page
 
             header("Location: index.php");
             exit();
-
-
-
-/*            session_destroy();
-            session_write_close();
-
-            session_id(base64_encode($username));
-            session_start();
-            $_SESSION['username'] = $username;
-            $_SESSION['uid'] = $rowUID;
-            $authcookie = base64_encode($username . $rowUID);
-            setcookie("AUTHID", $authcookie, time() + 60*15, "/");
-*/
-
         }
     }
-        else {
-            //echo "<div class='form'> <h2>Username or password is incorrect.</h2><br>Click here to <a href = 'login' Login</a></div>";
-
-            echo '<div class="form"><h1>Login</h1><form action="" method="post" name="login"><input type="text" name="username" placeholder="Username" required /><input type="password" name="password" placeholder="Password" required /><input name="submit" type="submit" value="Login" /></form> ';
-        }
+}
 
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+</head>
+
+<body>
+
+Not registered yet? Click <a href=register.php>here</a> to register
+<div class="form">
+    <h1>Login</h1>
+    <form action="" method="post" name="login">
+        <input type="text" name="username" placeholder="Username" required />
+        <input type="password" name="password" placeholder="Password" required />
+        <input name="submit" type="submit" value="Login" />
+    </form>
+
 </body>
 
 </html>
