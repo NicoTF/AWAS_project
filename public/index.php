b063a0cc6eb33dac65e4ed66ac1ef7539e84887e
Nicolò Tagliaferro <nico.taglia26@gmail.com>
2023-05-17 15:50:02 +0200

    Post list in index.php

--- public/index.php
+++ public/index.php
@@ -6,21 +6,15 @@ require_once '../tools/check_auth.php';
 ?>
 
 <html>
-
-
 <?php include '../tools/menu.php'; ?>
-    <h1> Welcome <?php echo $_SESSION['username'] ?>! </h1>
-<?php
-$posts = $DB->query("SELECT posts.*, username FROM posts inner join users u on u.id = posts.user_id order by posts.id desc");
-foreach ($posts as $post) {
-    ?>
-    <div class="post">
-        <h3>@<?php echo $post['username']; ?></h3>
-        <img src="/content/get_secured_picture.php?name=<?php echo base64_encode($post['image_path']); ?>"/>
-        <p><?php echo $post['description']; ?></p>
-    </div>
+<h1> Welcome <?php echo $_SESSION['username'] ?>! </h1>
+<div id="posts">
     <?php
-}
-?>
+    $posts = $DB->query("SELECT posts.*, username FROM posts inner join users u on u.id = posts.user_id order by posts.id desc");
+    foreach ($posts as $post) {
+        buildPost($post);
+    }
+    ?>
+</div>
 </body>
 </html>
