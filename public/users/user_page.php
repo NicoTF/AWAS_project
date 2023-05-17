c840fbd87041af672f78ef3cfdc797831c033fb1
Nicolò Tagliaferro <nico.taglia26@gmail.com>
2023-05-17 16:02:50 +0200

    User's posts page

---
+++ public/users/user_page.php
@@ -0,0 +1,32 @@
+<?php
+require_once '../../tools/db_conn.php';
+require_once '../../tools/utils.php';
+global $DB;
+
+$uid = $_GET['uid'];
+
+$query = $DB->prepare("SELECT username FROM users WHERE id = ?");
+$query->execute([$uid]);
+$username = $query->fetch()[0];
+
+
+
+$query = $DB->prepare("SELECT * FROM posts WHERE user_id = ?");
+$query->execute([$uid]);
+$posts = $query->fetchAll();
+
+?>
+<body>
+<?php
+include '../../tools/menu.php';
+echo "<h1>$username's posts</h1>";
+?>
+<div id="posts">
+    <?php
+    foreach ($posts as $post) {
+        buildPost($post);
+    }
+    ?>
+</div>
+</body>
+
