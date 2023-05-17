155c29c0d157858682b4fd642d4f104d7ba9beac
Nicolò Tagliaferro <nico.taglia26@gmail.com>
2023-05-17 16:03:07 +0200

    Show post for known user

--- tools/utils.php
+++ tools/utils.php
@@ -1,9 +1,17 @@
 <?php
 
-function buildPost($post) {
+function buildPost($post)
+{
     ?>
     <div class="post">
-        <h3>@<?php echo $post['username']; ?></h3>
+        <?php
+        if (isset($post['username'])) {
+            ?>
+            <a href="/users/user_page.php?uid=<?php echo $post['user_id']; ?>"><h3>
+                    @<?php echo $post['username']; ?></h3></a>
+            <?php
+        }
+        ?>
         <img src="/content/get_secured_picture.php?name=<?php echo base64_encode($post['image_path']); ?>"/>
         <p><?php echo $post['description']; ?></p>
     </div>
