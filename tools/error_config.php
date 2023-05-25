425b8165b45c11481960d7d248301807e2dfa75c
Nicolò Tagliaferro <nico.taglia26@gmail.com>
2023-05-25 15:43:55 +0200

    Print error

--- tools/error_config.php
+++ tools/error_config.php
@@ -1,2 +1,8 @@
 <?php
 error_reporting(0);
+
+// intercept errors and respond with a 500
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    echo "Something went wrong!";
+    exit(500);
+});
